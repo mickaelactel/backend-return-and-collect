2a60da3f03f4f159ff03b3ccdf699eac21b558b4
Marc Hourselle <hourselle.marc@gmail.com>
2025-02-03 21:23:30 +0100

    route signup marche d'apres le test

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+require('../models/connection');
+const User = require('../models/users');
+const bcrypt = require('bcrypt');
+
+router.post('/signup', (req, res) => {
+  if(!req.body.email || !req.body.password || !req.body.confirmPassword) {
+    res.json({result: false, error: "un champ de saisie est vide"})
+
+    const hash = bcrypt.hashSync(req.body.password, 10);
+
+  } else {
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    const newUser = new User({
+      email: req.body.email,
+      password: hash,
+    })
+    newUser.save().then((newData) => {
+      res.json({result: true})
+    })
+  }
+})
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
