b256f0cb0f2af6981779b2eb752d7f2414462ca5
Mickaël Adiduyulmus <mickael.adiduyulmus@actelior.com>
2025-02-22 09:47:22 +0100

    remove case sensitiveness on signup and login

--- routes/users.js
+++ routes/users.js
@@ -8,25 +8,29 @@ const uid2 = require("uid2");
 
 // User signup
 router.post("/signup", (req, res) => {
-  if (!req.body.email || !req.body.password || !req.body.confirmPassword) {
+  const { email, password, confirmPassword } = req.body;
+
+  if (!email || !password || !confirmPassword) {
     //si un des 3 champs est vide (!) l'erreur de la ligne 11 s'affiche
     res.json({ result: false, error: "un champ de saisie est vide" });
     return;
   }
-  if (!req.body.email.match(/.+\@.+\..+/)) {
+  if (!email.match(/.+\@.+\..+/)) {
     // si le nouvel utilisateur ne rentre pas un type email l'erreur de la ligne 15 s'affiche
     res.json({ result: false, error: "Email invalid" });
     return;
   }
 
-  User.findOne({ email: req.body.email }).then((data) => {
+  const lowerCaseEmail = email.toLowerCase();
+
+  User.findOne({ email: lowerCaseEmail }).then((data) => {
     if (data !== null) {
       // si on rentre un email qui est déjà stocker dans la bdd l'erreur de la ligne 22 s'affiche sinon on execute la ligne 25 et on crée un nouvel utilisateur
       res.json({ result: false, error: "Email already used" });
     } else {
       const hash = bcrypt.hashSync(req.body.password, 10);
       const newUser = new User({
-        email: req.body.email,
+        email: lowerCaseEmail,
         password: hash,
         token: uid2(32),
       });
@@ -77,19 +81,22 @@ router.put("/", (req, res) => {
 });
 
 router.post("/signin", (req, res) => {
-  if (!req.body.email || !req.body.password) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
     res.json({ result: false, error: "un champ de saisie est vide" });
     return;
   }
 
-  if (!req.body.email.match(/.+\@.+\..+/)) {
+  if (!email.match(/.+\@.+\..+/)) {
     res.json({ result: false, error: "Email invalid" });
     return;
   }
 
-  User.findOne({ email: req.body.email }).then((data) => {
+  const lowerCaseEmail = email.toLowerCase();
+  User.findOne({ email: lowerCaseEmail }).then((data) => {
     if (data) {
-      if (bcrypt.compareSync(req.body.password, data.password)) {
+      if (bcrypt.compareSync(password, data.password)) {
         res.json({ result: true, data: data });
       } else {
         res.json({ result: false, error: "Mot de passe incorrect" });
